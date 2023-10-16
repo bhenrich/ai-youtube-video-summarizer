61299d80ed0d1f70f4ab3a541d072596b6aa0c28
bhenrich <benjamin@bhenrich.de>
2023-10-16 13:49:09 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function Legal() {
 				<Content className='Legal-Content'>
 					<div className='Legal-Table'>
 						<div className='Legal-Table-Left'>
-							<Title level='4'>Impressum</Title>
+							<Title className='App-Title' level="4"><span className='fancy-text-gradient'>Impressum</span></Title>
 							<br />
 							<b>Website</b>
 							<br />
@@ -60,7 +60,7 @@ function Legal() {
 							</p>
 						</div>
 						<div className='Legal-Table-Right'>
-							<h1>Datenschutz&shy;erkl&auml;rung</h1>
+							<Title className='App-Title' level="4"><span className='fancy-text-gradient uppies'>Datenschutzerkl&auml;rung</span></Title>
 							<h2>1. Datenschutz auf einen Blick</h2>
 							<h3>Allgemeine Hinweise</h3> <p>Die folgenden Hinweise geben einen einfachen &Uuml;berblick dar&uuml;ber, was mit Ihren personenbezogenen Daten passiert, wenn Sie diese Website besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie pers&ouml;nlich identifiziert werden k&ouml;nnen. Ausf&uuml;hrliche Informationen zum Thema Datenschutz entnehmen Sie unserer unter diesem Text aufgef&uuml;hrten Datenschutzerkl&auml;rung.</p>
 							<h3>Datenerfassung auf dieser Website</h3> <h4>Wer ist verantwortlich f&uuml;r die Datenerfassung auf dieser Website?</h4> <p>Die Datenverarbeitung auf dieser Website erfolgt durch den Websitebetreiber. Dessen Kontaktdaten k&ouml;nnen Sie dem Abschnitt &bdquo;Hinweis zur Verantwortlichen Stelle&ldquo; in dieser Datenschutzerkl&auml;rung entnehmen.</p> <h4>Wie erfassen wir Ihre Daten?</h4> <p>Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese mitteilen. Hierbei kann es sich z.&nbsp;B. um Daten handeln, die Sie in ein Kontaktformular eingeben.</p> <p>Andere Daten werden automatisch oder nach Ihrer Einwilligung beim Besuch der Website durch unsere IT-Systeme erfasst. Das sind vor allem technische Daten (z.&nbsp;B. Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). Die Erfassung dieser Daten erfolgt automatisch, sobald Sie diese Website betreten.</p> <h4>Wof&uuml;r nutzen wir Ihre Daten?</h4> <p>Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung der Website zu gew&auml;hrleisten. Andere Daten k&ouml;nnen zur Analyse Ihres Nutzerverhaltens verwendet werden.</p> <h4>Welche Rechte haben Sie bez&uuml;glich Ihrer Daten?</h4> <p>Sie haben jederzeit das Recht, unentgeltlich Auskunft &uuml;ber Herkunft, Empf&auml;nger und Zweck Ihrer gespeicherten personenbezogenen Daten zu erhalten. Sie haben au&szlig;erdem ein Recht, die Berichtigung oder L&ouml;schung dieser Daten zu verlangen. Wenn Sie eine Einwilligung zur Datenverarbeitung erteilt haben, k&ouml;nnen Sie diese Einwilligung jederzeit f&uuml;r die Zukunft widerrufen. Au&szlig;erdem haben Sie das Recht, unter bestimmten Umst&auml;nden die Einschr&auml;nkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Des Weiteren steht Ihnen ein Beschwerderecht bei der zust&auml;ndigen Aufsichtsbeh&ouml;rde zu.</p> <p>Hierzu sowie zu weiteren Fragen zum Thema Datenschutz k&ouml;nnen Sie sich jederzeit an uns wenden.</p>
@@ -175,8 +175,8 @@ function Terms() {
 							<p>By using the Service, you agree that the generated summaries are for:</p>
 							<ul>
 								<li>Private use only</li>
+								<li>Limited redistribution <i>(See Section "Intellectual Property" of the Terms of Service)</i></li>
 								<li>No commercial use</li>
-								<li>No redistribution</li>
 								<li>No modification</li>
 							</ul>
 
